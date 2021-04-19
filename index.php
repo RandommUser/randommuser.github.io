30a57814bdbb929e09fef4f26dd0c6e03e8ba26a
RandommUser <RandommUser@gmail.com>
2021-04-19 10:39:17 +0300

    Start of the page

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Petrus Hakakoski works</title>
+<meta charset=UTF-8">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<b>Welcome!</b>
+<br><br>
+Unfortunately I haven't had the time to finish this page yet.<br>
+But here are my links for you to check out
+<br>
+<a href="https://www.linkedin.com/in/petrushakakoski/" title="LinkedIn" alt="LinkedIn"></a>
+<a href="https://github.com/RandommUser" title="Github" alt="Github"></a>
+</body>
+</html>
